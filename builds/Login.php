e64751a6641e04d859db0cf64faa2641c5b4bfaa
Bhumika Shukla <b.shukla@ilead-synapse.com>
2014-07-29 15:23:04 +0530

    changes

--- builds/Login.php
+++ builds/Login.php
@@ -1,12 +1,12 @@
 <?php
 class Login {
-   // protected $name;
-    /*public function getName() {
+    protected $name;
+    public function getName() {
         return $this->name;
     }
     public function setName($name) {
 			$this->name = $name;
-    }*/
+    }
     public function check_login() {
         return "123";
     }
